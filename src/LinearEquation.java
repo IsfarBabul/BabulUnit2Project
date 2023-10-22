cb1161b15ba21dab6ed0d5511ea55c41f98b5447
Isfar Babul <isfarb2@nycstudents.net>
2023-10-22 12:17:59 -0400

    Initial logic

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class LinearEquation {
+import java.text.DecimalFormat;
+
+public class LinearEquation {
+
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public LinearEquation(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return roundedToHundredth(distance);
+    }
+
+
+
+    public double roundedToHundredth(double toRound) {
+        toRound = Math.floor(toRound * 100) / 100;
+        return toRound;
+    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+
+public class LinearEquationLogic {
+
+    Scanner scan = new Scanner(System.in);
+
+    public LinearEquationLogic() { }
+
+    public void start() {
+    }
+
+    public void printData() {
+        System.out.print("Enter coordinate 1: ");
+        String point1 = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        String point2 = scan.nextLine();
+        int x1 = Integer.parseInt(point1.substring(point1.indexOf("(") + 1, point1.indexOf(",")));
+        int x2 = Integer.parseInt(point1.substring(point1.indexOf(" ") + 1, point1.indexOf(")")));
+        int y1 = Integer.parseInt(point2.substring(point1.indexOf("(") + 1, point1.indexOf(",")));
+        int y2 = Integer.parseInt(point2.substring(point1.indexOf(" ") + 1, point1.indexOf(")")));
+        LinearEquation pointsInfo = new LinearEquation(x1, x2, y1, y2);
+    }
+
+    public void printX() {
+
+    }
+
 }
