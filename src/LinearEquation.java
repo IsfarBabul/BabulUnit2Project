f9b789d9301000c54a2a11554dbb8105e9841e50
Isfar Babul <isfarb2@nycstudents.net>
2023-10-31 22:23:32 -0400

    majority done

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -15,14 +15,39 @@ public class LinearEquation {
     }
 
     public double distance() {
-        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return roundedToHundredth(distance);
+        return roundedToHundredth(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
     }
 
+    public double yIntercept() {
+        return roundedToHundredth(y1 - slope() / x1);
+    }
 
+    public double slope() {
+        return roundedToHundredth((double) (y2 - y1) / (x2 - x1));
+    }
 
-    public double roundedToHundredth(double toRound) {
-        toRound = Math.floor(toRound * 100) / 100;
-        return toRound;
+    public String equation() {
+        if (y2 - y1 != 0) {
+          return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x + " + yIntercept();
+        } else {
+          return "y = " + yIntercept();
+        }
+    }
+
+    public String coordinateForX(double x) {
+        return "The point on the line is (" + x + ", " + (slope() * x + yIntercept()) + ")";
+    }
+
+    public String lineInfo() {
+        String pointsInfo = "The two points are: (" + x1 + ", " + x2 + ") and (" + y1 + ", " + y2 + ")";
+        String equationInfo = "The equation of the line between these points is: " + equation();
+        String slopeInfo = "The slope of this line is: " + slope();
+        String yInterceptInfo = "The y-intercept of this line is " + yIntercept();
+        String distanceInfo = "The distance between these points is " + distance();
+        return "\n" + pointsInfo + "\n" + equationInfo + "\n" + slopeInfo + "\n" + yInterceptInfo + "\n" + distanceInfo + "\n";
+    }
+
+    private double roundedToHundredth(double toRound) {
+        return Math.floor(toRound * 100) / 100;
     }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,28 +1,53 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class LinearEquationLogic {
 
     Scanner scan = new Scanner(System.in);
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+    String point1;
+    String point2;
+    double x;
+    LinearEquation function = new LinearEquation(x1, x2, y1, y2);
 
     public LinearEquationLogic() { }
 
     public void start() {
+        collectPoints();
+        parsePoints(point1, point2);
+        pointsInfo();
+        collectX();
+        printX(x);
     }
 
-    public void printData() {
+    private void collectPoints() {
+        System.out.println("Welcome to the  linear equation calculator!");
         System.out.print("Enter coordinate 1: ");
-        String point1 = scan.nextLine();
+        point1 = scan.nextLine();
         System.out.print("Enter coordinate 2: ");
-        String point2 = scan.nextLine();
-        int x1 = Integer.parseInt(point1.substring(point1.indexOf("(") + 1, point1.indexOf(",")));
-        int x2 = Integer.parseInt(point1.substring(point1.indexOf(" ") + 1, point1.indexOf(")")));
-        int y1 = Integer.parseInt(point2.substring(point1.indexOf("(") + 1, point1.indexOf(",")));
-        int y2 = Integer.parseInt(point2.substring(point1.indexOf(" ") + 1, point1.indexOf(")")));
-        LinearEquation pointsInfo = new LinearEquation(x1, x2, y1, y2);
+        point2 = scan.nextLine();
     }
 
-    public void printX() {
-
+    private void parsePoints(String point1, String point2) {
+        x1 = Integer.parseInt(point1.substring(point1.indexOf("(") + 1, point1.indexOf(",")));
+        y1 = Integer.parseInt(point1.substring(point1.indexOf(" ") + 1, point1.indexOf(")")));
+        x2 = Integer.parseInt(point2.substring(point1.indexOf("(") + 1, point2.indexOf(",")));
+        y2 = Integer.parseInt(point2.substring(point1.indexOf(" ") + 1, point2.indexOf(")")));
     }
 
+    private void pointsInfo() {
+        System.out.println(function.lineInfo());
+    }
+
+    private void collectX() {
+        System.out.print("Enter a value for x: ");
+        x = scan.nextDouble();
+    }
+
+    private void printX(double x) {
+        System.out.println(function.coordinateForX(x));
+    }
 }
